ca13265c855173a7d62a2cfc6d1771091e7ab84a
Omar Negron <omarnegron92@hotmail.com>
2021-11-13 00:42:06 -0500

    setup db context

--- PlatformService/Startup.cs
+++ PlatformService/Startup.cs
@@ -31,6 +31,7 @@ namespace PlatformService
             services.AddDbContext<AppDbContext>(opt => 
                 opt.UseInMemoryDatabase("InMem"));
 
+            services.AddScoped<IPlatformRepo, PlatformRepo>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -58,6 +59,8 @@ namespace PlatformService
             {
                 endpoints.MapControllers();
             });
+
+            PrepDb.Seed(app);
         }
     }
 }
