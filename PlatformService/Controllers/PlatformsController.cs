3d51c6c7c2920f59414247767b9bc44bd848d11f
onegronm <omarnegron92@hotmail.com>
2021-11-16 14:23:47 -0500

    created platforms controller

--- PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/Controllers/PlatformsController.cs
@@ -1,12 +1,58 @@
+using System.Collections.Generic;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Models;
+using PlatformService.Models.DTOs;
 
 namespace PlatformService.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class PlatformsController : ControllerBase
     {
-        public PlatformsController(IPlatformRepo)
+        private readonly IPlatformRepo _repository;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(IPlatformRepo repository, IMapper mapper)
         {
-            
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDTO>> GetPlatforms()
+        {
+            var platformItem = _repository.GetAllPlatforms();
+
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDTO>>(platformItem));
+        }
+
+        [HttpGet("{id}", Name = "GetPlatformById")]
+        public ActionResult<PlatformReadDTO> GetPlatformById(int id)
+        {
+            var platformItem = _repository.GetPlatformById(id);
+            if(platformItem != null) 
+            {
+                return Ok(_mapper.Map<PlatformReadDTO>(platformItem));
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult<PlatformReadDTO> CreatePlatform([FromBody]PlatformCreateDTO platform)
+        {
+            var platformModel = _mapper.Map<Platform>(platform);
+            _repository.CreatePlatform(platformModel);
+            _repository.SaveChanges();
+
+            var platformReadDto = _mapper.Map<PlatformReadDTO>(platformModel);
+            return CreatedAtRoute(
+                nameof(GetPlatformById),                        
+                new { Id = platformReadDto },                   
+                platformReadDto                 
+            );
         }
     }
 }
