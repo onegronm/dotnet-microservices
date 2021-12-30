390a1444be5f0f6809fce31f90dc869068962f94
Omar Negron <omarnegron92@hotmail.com>
2021-12-30 15:51:47 -0500

    added commands service model

---
+++ CommandsService/DTOs/CommandCreateDto.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CommandsService.DTOs {
+    public class CommandCreateDto {
+        [Required]
+        public string HowTo { get; set; }
+        [Required]
+        public string CommandLine { get; set; }
+    }
+}
---
+++ CommandsService/DTOs/PlatformReadDto.cs
@@ -0,0 +1,7 @@
+namespace CommandsService.DTOs {
+    public class PlatformReadDto {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+    }
+}
