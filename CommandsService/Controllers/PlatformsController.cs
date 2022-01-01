7ac8dcdd19c8baeee5c473b39c5d0ff75f44508c
Omar Negron <omarnegron92@hotmail.com>
2022-01-01 14:38:58 -0500

    updated platforms controller

--- CommandsService/Controllers/PlatformsController.cs
+++ CommandsService/Controllers/PlatformsController.cs
@@ -1,18 +1,70 @@
 using System;
+using System.Collections.Generic;
+using AutoMapper;
+using CommandsService.Data;
+using CommandsService.DTOs;
+using CommandsService.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommandsService.Controllers {
     [Route("api/c/[controller]")]
     [ApiController]
     public class PlatformsController : ControllerBase {
-        public PlatformsController()
+        private readonly ICommandRepo _repository;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(ICommandRepo repository, IMapper mapper)
         {
-            
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<CommandReadDto>> GetCommandsForPlatform(int platformId)
+        {
+            if(!_repository.PlatformExists(platformId)) {
+                return NotFound();
+            }
+
+            var commands = _repository.GetCommandsForPlatform(platformId);
+
+            return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commands));
+        }
+
+        [HttpGet("{commandId}", Name = "GetCommandForPlatform")]
+        public ActionResult<CommandReadDto> GetCommandForPlatform(int platformId, int commandId) 
+        {
+            if(!_repository.PlatformExists(platformId)) {
+                return NotFound();
+            }
+
+            var command = _repository.GetCommand(platformId, commandId);
+
+            if (command == null) {
+                return NotFound();
+            }
+
+            return Ok(_mapper.Map<CommandReadDto>(command));
         }
 
         [HttpPost]
-        public ActionResult TestInboundConnection() {
-            return Ok("Inbound test from Platforms Controller");
+        public ActionResult<CommandReadDto> CreateCommandForPlatform(int platformId, CommandCreateDto dto) {
+            if(!_repository.PlatformExists(platformId)) 
+            {
+                return NotFound();
+            }
+
+            var command = _mapper.Map<Command>(dto);
+
+            _repository.CreateCommand(platformId, command);
+            _repository.SaveChanges();
+
+            var commandReadDto = _mapper.Map<CommandReadDto>(command);
+
+            return CreatedAtRoute(
+                nameof(GetCommandForPlatform),
+                new { platformId = platformId, commandID = commandReadDto.Id }, 
+                commandReadDto);
         }
     }
 }
